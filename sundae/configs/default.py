fbcb5b316a1e8e02553e5199e8d8e58d773a91ce
1ucky40nc3 <louisnwendler@gmail.com>
2023-04-15 13:45:27 +0000

    added new model and training config

--- sundae/configs/default.py
+++ sundae/configs/default.py
@@ -12,8 +12,13 @@ def get_config() -> mlc.ConfigDict:
         name="ffhq256",
         batch_size=48,  # TODO: really this shouldn't be under data, it affects the numerics of the model
         num_workers=4,
+        image_size=64,
+        train_dir=None,
+        validation_dir=None
     )
     config.model = dict(
+        model_name_or_path=None,
+        
         num_tokens=16_384,
         dim=1024,
         depth=[2, 12, 2],
@@ -26,11 +31,19 @@ def get_config() -> mlc.ConfigDict:
         parallel_block=True,
         tied_embedding=False,
         dtype=jnp.bfloat16,
+        cache_dir=None,
+        use_auth_token=None,
     )
     config.training = dict(
         learning_rate=1e-4,
         unroll_steps=2,
-        epochs=100,  # TODO: maybe replace with train steps
+        num_epochs=100,
+        max_steps=None,
+        warmup_steps=0,
+        adam_beta1=0.9,
+        adam_beta2=0.999,
+        adam_epsilon=1e-08,
+        weight_decay=0.0
     )
     config.vqgan = dict(
         name="vq-f16", 
@@ -38,5 +51,10 @@ def get_config() -> mlc.ConfigDict:
     )
 
     config.jit_enabled = True
+
+    config.logging_dir = "./logs"
+    config.report_to = [
+        "tensorboard"
+    ]
 
     return config
