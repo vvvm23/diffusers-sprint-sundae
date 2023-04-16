0f810d4b27a6eb22f8364ed2f59c37f0cf9286d4
1ucky40nc3 <louisnwendler@gmail.com>
2023-04-16 13:49:25 +0000

    fixes for hf unconditional datasets

--- sundae/datasets.py
+++ sundae/datasets.py
@@ -1,4 +1,5 @@
 from typing import (
+    Any,
     List,
     Dict,
     Union,
@@ -6,13 +7,18 @@ from typing import (
     Callable
 )
 
+import os
 import math
+import urllib
+import logging
+import requests
 
 import numpy as np
 
 import torch
 import torchvision
 from torchvision import transforms
+import torchvision.transforms.functional as TF
 
 from PIL import Image
 
@@ -27,22 +33,37 @@ from datasets import (
 import ml_collections as mlc
 
 
+logger = logging.getLogger(__name__)
+
+
 ImageTransform = Callable[[Union[Image.Image, np.ndarray, torch.Tensor]], torch.Tensor]
 
+
+def is_url(path: str) -> bool:
+    return urllib.parse.urlparse(url).scheme != ""
+
+def read_image(path: str) -> Image.Image:
+    if is_url(image_path):
+        image_data = requests.get(image_path, stream=True).raw
+        image = Image.open(image_data).convert("RGB")
+    else:
+        image = Image.open(image_path).convert("RGB")
+    return image
 
 class ImageTransform(torch.nn.Module):
     def __init__(self, transforms: List[torch.nn.Module]) -> None:
         super().__init__()
         self.transforms = torch.nn.Sequential(*transforms)
 
-    def forward(self, x: Image.Image) -> torch.Tensor:
+    def forward(self, x) -> torch.Tensor:
+        '''Transform x of type `PIL.Image.Image`.'''
         with torch.no_grad():
             x = self.transforms(x)
             x = x / 127.5 - 1.0
         return x
 
 
-def train_image_transforms(config: mlc.ConfigDict) -> List[torch.nn.Module]:
+def training_image_transforms(config: mlc.ConfigDict) -> List[torch.nn.Module]:
     return [
         transforms.RandomResizedCrop(config.data.image_size, antialias=True),
         transforms.RandomHorizontalFlip(config.data.flip_p),
@@ -54,6 +75,16 @@ def validation_image_transforms(config: mlc.ConfigDict) -> List[torch.nn.Module]
         transforms.Resize([config.data.image_size], interpolation=InterpolationMode.BICUBIC),
         transforms.CenterCrop(config.data.image_size),
     ]
+
+
+def jitted_image_transform(config: mlc.ConfigDict, split: Optional[str] = None) -> torch.nn.Module:
+    if split == "train":
+        transforms = training_image_transforms(config)
+    else:
+        transforms = validation_image_transforms(config)    
+    image_transform = ImageTransform(transforms)
+    image_transform = torch.jit.script(image_transform)
+    return image_transform
 
 
 def hf_dataset_length(dataset: Union[Dataset, IterableDataset]) -> Union[float, int]:
@@ -76,7 +107,9 @@ def load_hf_unconditional(
     num_processes: Optional[int] = None,
     max_samples: Optional[Union[int, Dict[str, int]]] = None,
     transform_images: Union[ImageTransform, Dict[str, ImageTransform]] = None,
-    splits: Optional[List[str]] = None
+    splits: Optional[List[str]] = None,
+    image_column: str = "image",
+    overwrite_cache: bool = False
 ) -> Union[DatasetDict, IterableDatasetDict]:
     # Load the dataset dict
     datasets = load_dataset(
@@ -96,11 +129,11 @@ def load_hf_unconditional(
         max_samples = {key: max_samples for key in datasets.keys()}
     if not isinstance(transform_images, dict):
         transform_images = {key: transform_images for key in datasets.keys()}
-    if split is None:
+    if splits is None:
         splits = list(datasets.keys())
 
     preprocessed_datasets = datasets.__class__()
-    for split, dataset in enumerate(datasets):
+    for split, dataset in datasets.items():
         if split not in splits:
             continue
         
@@ -111,7 +144,44 @@ def load_hf_unconditional(
             max_samples_split = min(hf_dataset_length(dataset), max_samples_split)
             dataset = dataset.select(range(max_samples_split))
         if transform_images_split is not None:
-            dataset.set_transform(transform_images_split)
+            if isinstance(dataset, IterableDataset):
+                def preprocessing_fn(examples: Dict[str, Any]) -> Dict[str, Any]:
+                    images = []
+                    discarded = []
+                    for image in examples[image_column]:
+                        try:
+                            if isinstance(image, str):
+                                image = read_image(image)
+                            image = TF.to_tensor(image)
+                            image = transform_images_split(image)
+                            images.append(image)
+                            discarded.append(False)
+                        except Exception as e:
+                            logger.info(f'Caught: "{e}" during image reading.')
+                            discarded.append(True)
+                    
+                    # Discard samples were couldn't read the image
+                    # for name, values in examples.items():
+                    #     if name != image_column:
+                    #         tmp = []
+                    #         for discard, value in zip(discarded, values):
+                    #             if not discard:
+                    #                 tmp.append(value)
+                    #         examples[name] = tmp
 
+                    examples["pixel_values"] = images
+                    return examples
+
+                dataset = dataset.remove_columns([c for c in dataset.column_names if c != image_column])
+                dataset = dataset.map(
+                    preprocessing_fn,
+                    batched=True,
+                    remove_columns=[image_column]
+                )
+            else:
+                # The dataset is of type `Dataset`
+                dataset = dataset.remove_columns([c for c in dataset.column_names if c != image_column])
+                dataset.set_transform(transform_images_split)
+            
         preprocessed_datasets[split] = dataset
     return preprocessed_datasets
